6dc73c3a1ef9a7575460e34f0e551876a876b699
eduardoscsouza@gmail.com <eduardoscsouza@gmail.com>
2019-12-03 14:32:56 -0300

    Add baseline experment

---
+++ run_baseline_model.py
@@ -0,0 +1,21 @@
+from tensorflow.keras.backend import clear_session
+import build_model
+import training
+import os
+
+
+
+expr_out_dir = "experiments"
+logs_out_dir = "tensorboard_logs"
+extracted_dir = "extracted"
+
+for ext_lay in range(3):
+    exp_name = "base_outLay:{}".format(ext_lay)
+    if not os.path.isdir(os.path.join(expr_out_dir, exp_name)):
+        clear_session()
+        input_size = 2622 if ext_lay == 2 else 4096
+        model = build_model.build_base_model(input_size)
+        training.run_experiment(model, os.path.join(extracted_dir, "layer_{}".format(ext_lay)), exp_name,
+                    experiments_dir=expr_out_dir, tensorboard_logs_dir=logs_out_dir,
+                    epochs=0, steps_per_epoch=0, validation_steps=0, early_stop_patience=0,
+                    evaluation_steps=500)
