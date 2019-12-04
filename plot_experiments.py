71f71f2856404c2c6b4663271ab7ffa505e297c3
eduardoscsouza@gmail.com <eduardoscsouza@gmail.com>
2019-12-04 15:04:01 -0300

    Update plots file

--- plot_experiments.py
+++ plot_experiments.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 
 
-def plot_columns(df, cols, out_filename="plot.html", tooltips=None, colors=['blue', 'red', 'purple', 'orange', 'yellow', 'green']):
+def plot_columns(df, cols, out_filename="plot.html", tooltips=None,
+            colors=['blue', 'red', 'purple', 'black', 'yellow', 'green']):
+    df = df.copy()
     col_vals = [tuple((str(df.iloc[i][col]) for col in cols)) for i in range(len(df))]
     aux_splits = [str(col_val).split(',') for col_val in col_vals]
     aux_splits = [[word.strip("()', ") for word in pair if word.strip("()', ")] for pair in aux_splits]
@@ -37,6 +39,21 @@ tooltips = [
     ("Binary Accuracy Mean", "@{Binary Accuracy Mean}"),
     ("Binary Accuracy Std", "@{Binary Accuracy Std}")]
 
-plot_columns(df, ["Distance Function Type", "Combination Function Level"], tooltips=tooltips)
-plot_columns(df, ["Distance Function Type"], tooltips=tooltips)
-plot_columns(df, ["# of Layers"], tooltips=tooltips)
+#Transfer melhor
+plot_columns(df, ["Extraction Type"], tooltips=tooltips, out_filename="extraction_type.html")
+
+#Aparenta bem distribuido
+df = df[df["Extraction Type"] == "Transfer"]
+plot_columns(df, ["Distance Function Type"], tooltips=tooltips, out_filename="eucl_vs_cos.html")
+
+#3 e 2 quase tudo alto, os outros distribuidos. 3 e 2 melhor
+plot_columns(df[df["Extraction Type"] == "Transfer"], ["Combination Function Level"],
+tooltips=tooltips, out_filename="levels.html")
+
+#Todos bons, mas cosseno um pouco melhor
+df = df[df["Combination Function Level"] >= 3]
+plot_columns(df, ["Extraction Type", "Extraction Layer"], tooltips=tooltips, out_filename="layers.html")
+
+#Melhores
+df = df[df["Distance Function Type"] == "Cosine"]
+plot_columns(df, ["Distance Function Type"], tooltips=tooltips, out_filename="tops.html")
